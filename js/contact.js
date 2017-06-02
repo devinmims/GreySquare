a9a6155b392a4d88236d41390c686453be5c0a42
devinmims <devinmims@gmail.com>
2017-06-01 21:01:50 -0400

    Added page
    Added userbeers and corrected merge conflict

--- js/contact.js
+++ js/contact.js
@@ -1,48 +1,48 @@
-// Validating Empty Field
-function check_empty()
-{
-    if (document.getElementById('name').value == "" || document.getElementById('email').value == "" || document.getElementById('msg').value == "")
-    {
-        alert("Fill All Fields !");
-    } else
-    {
-        //document.getElementById('form').submit();
-        //alert("Form Submitted Successfully...");
-        var email = {
-            toEmailAddress: "contact@greysquaretechnologies.com",
-            fromEmailAddress: document.getElementById('email').value,
-            subject: "Email from GreySquare",
-            message: document.getElementById('msg').value,
-            name: document.getElementById('name').value
-        }
-        sendEmail(email);
-    }
-}
-//Function To Display Popup
-function div_show()
-{
-    document.getElementById('abc').style.display = "block";
-}
-//Function to Hide Popup
-function div_hide()
-{
-    document.getElementById('abc').style.display = "none";
-}
-
-function sendEmail(email)
-{
-    var urlToApi = "http://www.greysquaretechnologies.com:5000/api";
-    $.ajax({
-            method: "POST",
-            contentType: "application/json; charset=utf-8",
-            data: JSON.stringify(email),
-            url: urlToApi + "/email/",
-            success: function(data) {
-                document.getElementById('abc').style.display = "none";
-            },
-            error: function(jqXHR, textStatus, errorThrown){
-                console.log(textStatus);
-                alert("There was like, an error doing that");
-            }
-        });
-}
+// Validating Empty Field
+function check_empty()
+{
+    if (document.getElementById('name').value == "" || document.getElementById('email').value == "" || document.getElementById('msg').value == "")
+    {
+        alert("Fill All Fields !");
+    } else
+    {
+        //document.getElementById('form').submit();
+        //alert("Form Submitted Successfully...");
+        var email = {
+            toEmailAddress: "contact@greysquaretechnologies.com",
+            fromEmailAddress: document.getElementById('email').value,
+            subject: "Email from GreySquare",
+            message: document.getElementById('msg').value,
+            name: document.getElementById('name').value
+        }
+        sendEmail(email);
+    }
+}
+//Function To Display Popup
+function div_show()
+{
+    document.getElementById('abc').style.display = "block";
+}
+//Function to Hide Popup
+function div_hide()
+{
+    document.getElementById('abc').style.display = "none";
+}
+
+function sendEmail(email)
+{
+    var urlToApi = "http://www.greysquaretechnologies.com:5000/api";
+    $.ajax({
+            method: "POST",
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify(email),
+            url: urlToApi + "/email/",
+            success: function(data) {
+                document.getElementById('abc').style.display = "none";
+            },
+            error: function(jqXHR, textStatus, errorThrown){
+                console.log(textStatus);
+                alert("There was like, an error doing that");
+            }
+        });
+}
